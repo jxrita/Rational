756f1e1a0981654a2cef77b68894754a93e6e987
Chanyanud Suwannimit <88829651+jxrita@users.noreply.github.com>
2023-09-20 15:37:22 +0700

    JUnit Exercise Updated

--- src/main/java/Rational.java
+++ src/main/java/Rational.java
@@ -14,6 +14,9 @@ class Rational {
 
     Rational(long numerator, long denominator) throws Illegal { 
         // to be completed
+        this.numerator = numerator;
+        this.denominator = denominator;
+        simplestForm();
     } 
 
     // find the reduce form 
@@ -46,6 +49,9 @@ class Rational {
      */
     public void subtract(Rational x) {
         // to be completed
+        numerator = (numerator * x.denominator) - (x.numerator * denominator);
+        denominator = (denominator * x.denominator);
+        simplestForm();
     }
 
     /***
@@ -54,6 +60,9 @@ class Rational {
      */
     public void multiply(Rational x) { 
         // to be completed
+        numerator = numerator * x.numerator;
+        denominator = denominator * x.denominator;
+        simplestForm();
     }
 
     /***
@@ -62,6 +71,9 @@ class Rational {
      */
     public void divide(Rational x) {
         // to be completed
+        numerator = numerator * x.denominator;
+        denominator = denominator * x.numerator;
+        simplestForm();
     }
 
     /***
@@ -71,7 +83,13 @@ class Rational {
      */
     public boolean equals(Object x) {
         // to be completed
-        return true; // TODO: This needs to be modified.
+        Rational r = (Rational) x;
+        r.simplestForm();
+        if(this.numerator == numerator && this.denominator == denominator) {
+            return true;
+        }else {
+            return false;
+        }
     }
 
     /***
--- src/test/java/RationalTest.java
+++ src/test/java/RationalTest.java
@@ -13,4 +13,40 @@ public class RationalTest {
         x.add(y);
         Assert.assertEquals(3, x.numerator);
     }
+    @Test
+    public void testSubstract() throws Rational.Illegal{
+        Rational x = new Rational();
+        x.numerator = 1;
+        x.denominator = 5;
+        Rational y = new Rational();
+        y.numerator = 1;
+        y.denominator = 4;
+        x.subtract(y);
+        Assert.assertEquals(-1, x.numerator);
+        Assert.assertEquals(20, x.denominator);
+    }
+    @Test
+    public void testMultiply() throws Rational.Illegal{
+        Rational x = new Rational();
+        x.numerator = 2;
+        x.denominator = 5;
+        Rational y = new Rational();
+        y.numerator = 1;
+        y.denominator = 4;
+        x.multiply(y);
+        Assert.assertEquals(1, x.numerator);
+        Assert.assertEquals(10, x.denominator);
+    }
+    @Test
+    public void testEqual() throws Rational.Illegal{
+        Rational x = new Rational();
+        x.numerator = 2;
+        x.denominator = 4;
+        Rational y = new Rational();
+        y.numerator = 2;
+        y.denominator = 4;
+        x.equals(y);
+        Assert.assertEquals(2, x.numerator);
+        Assert.assertEquals(4, x.denominator);
+    }
 }
